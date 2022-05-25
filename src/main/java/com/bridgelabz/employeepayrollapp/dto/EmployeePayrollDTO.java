0800640a5c6112d9ecd105793624d185ad4454c8
Naveen-2 <naveen2k15cse@gmail.com>
2022-05-25 11:36:32 +0530

    Validations done and exceptions handled for new data fields

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -20,14 +23,20 @@ public @ToString class EmployeePayrollDTO {
     @Min(value = 500, message = "Minimum wage should be more than 500")
     public long salary;
 
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
 
     @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start date should not be empty")
+    @PastOrPresent(message = "Start date should be past or today's date")
     public LocalDate startDate;
 
+    @NotBlank(message = "Note cannot be empty")
     public String note;
 
+    @NotBlank(message = "Profile pic cannot be empty")
     public String profilePic;
 
+    @NotNull(message = "Department cannot be empty")
     public List<String> department;
 }
